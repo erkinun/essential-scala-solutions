11b23c8e5f7b4acfad53908412b9be68e71049e3
Erkin <erkinun@gmail.com>
2019-04-23 21:15:14 +0100

    5.4.6.2 Folding Maybe

--- src/main/scala/com/asyaminor/generics/Maybe.scala
+++ src/main/scala/com/asyaminor/generics/Maybe.scala
@@ -1,6 +1,12 @@
 package com.asyaminor.generics
 
 // 5.4.4.1 Exercise: Maybe that Was a Mistake
-sealed trait Maybe[T]
+sealed trait Maybe[T] {
+  // 5.4.6.2 Folding Maybe
+  def fold[U](empty: => U)(full: T => U): U = this match {
+    case Empty() => empty()
+    case Full(value) => full(value)
+  }
+}
 case class Full[T](value: T) extends Maybe[T]
 case class Empty[T]() extends Maybe[T]
