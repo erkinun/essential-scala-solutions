ce16737d1577d5000535766e605ba616be2be3de
Erkin <erkinun@gmail.com>
2019-04-23 21:32:58 +0100

    5.5.2 FlatMap

--- src/main/scala/com/asyaminor/generics/Maybe.scala
+++ src/main/scala/com/asyaminor/generics/Maybe.scala
@@ -2,6 +2,13 @@ package com.asyaminor.generics
 
 // 5.4.4.1 Exercise: Maybe that Was a Mistake
 sealed trait Maybe[T] {
+
+  // 5.5.2 FlatMap
+  def flatMap[U](fn: T => Maybe[U]): Maybe[U] = this match {
+    case Empty() => Empty[U]()
+    case Full(value) => fn(value)
+  }
+
   // 5.4.6.2 Folding Maybe
   def fold[U](empty: => U)(full: T => U): U = this match {
     case Empty() => empty()
