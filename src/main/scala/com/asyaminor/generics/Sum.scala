3145e132b5ad8170074042b21f0bfbeec7414ee2
Erkin <erkinun@gmail.com>
2019-04-23 21:16:58 +0100

    5.4.6.3 Folding Sum

--- src/main/scala/com/asyaminor/generics/Sum.scala
+++ src/main/scala/com/asyaminor/generics/Sum.scala
@@ -1,6 +1,12 @@
 package com.asyaminor.generics
 
 // 5.4.3.1 Exercise: Generic Sum Type
-sealed trait Sum[T, U]
+sealed trait Sum[T, U] {
+  // 5.4.6.3 Folding Sum
+  def fold[Z](left: T => Z)(right: U => Z): Z = this match {
+    case Left(value) => left(value)
+    case Right(value) => right(value)
+  }
+}
 case class Left[T, U](value: T) extends Sum[T, U]
 case class Right[T, U](value: U) extends Sum[T, U]
