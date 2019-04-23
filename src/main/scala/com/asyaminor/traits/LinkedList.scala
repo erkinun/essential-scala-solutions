53d6cfd677a6b4d947be2ab40e0e9bf920dc055a
Erkin <erkinun@gmail.com>
2019-04-23 21:28:05 +0100

    5.5.1 Map

--- src/main/scala/com/asyaminor/traits/LinkedList.scala
+++ src/main/scala/com/asyaminor/traits/LinkedList.scala
@@ -2,6 +2,13 @@ package com.asyaminor.traits
 
 // 5.1.3.1 Generic List
 sealed trait LinkedList[A] {
+
+  // 5.5.1 Map
+  def map[B](fn: A => B): LinkedList[B] = this match {
+    case End() => End()
+    case Pair(h, t) => Pair(fn(h), t.map(fn))
+  }
+
   // 4.6.3.1 A List of Methods
   def length: Int = fold(0, (_, len: Int) => len + 1)
 
