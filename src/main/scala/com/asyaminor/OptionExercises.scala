bfad4ad5c3fc5610f7d0a50d73341b4cf7fd0444
Erkin <erkinun@gmail.com>
2019-04-30 21:54:11 +0100

    6.5.1.2 Adding All of the Things

--- src/main/scala/com/asyaminor/OptionExercises.scala
+++ src/main/scala/com/asyaminor/OptionExercises.scala
@@ -12,4 +12,13 @@ object OptionExercises {
   def addOptions2(opt1: Option[Int], opt2: Option[Int]): Option[Int] = {
     opt1.flatMap(a => opt2.map(b => a + b))
   }
+
+  // 6.5.1.2 Adding All of the Things
+  def addOptions(opt1: Option[Int], opt2: Option[Int], opt3: Option[Int]): Option[Int] = {
+    for {
+      val1 <- opt1
+      val2 <- opt2
+      val3 <- opt3
+    } yield val1 + val2 + val3
+  }
 }
