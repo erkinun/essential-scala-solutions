6a58bffeb9bb30b84c16edb399fdbf5f668d10be
Erkin <erkinun@gmail.com>
2019-05-04 14:22:43 +0100

    6.8.4.2 Union of Maps

--- src/main/scala/com/asyaminor/CollectionExercises.scala
+++ src/main/scala/com/asyaminor/CollectionExercises.scala
@@ -8,4 +8,14 @@ object CollectionExercises {
       else { acc + item }
     })
   }
+
+  // 6.8.4.2 Union of Maps
+  def union[T](map1: Map[T, Int], map2: Map[T, Int]): Map[T, Int] = {
+    map1.foldLeft(map2)((m, item) => {
+      m.get(item._1) match {
+        case Some(i) => m.updated(item._1, i + item._2)
+        case None => m + item
+      }
+    })
+  }
 }
