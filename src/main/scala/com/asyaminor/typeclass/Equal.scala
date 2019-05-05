60b4c2d0a8cb88083ea043af39168c65c0dd98a7
Erkin <erkinun@gmail.com>
2019-05-05 14:06:08 +0100

    7.6.3 Easy Equality

--- src/main/scala/com/asyaminor/typeclass/Equal.scala
+++ src/main/scala/com/asyaminor/typeclass/Equal.scala
@@ -4,3 +4,15 @@ package com.asyaminor.typeclass
 trait Equal[A] {
   def equal(a1: A, a2: A): Boolean
 }
+
+// 7.6.3 Easy Equality
+object Equal {
+  implicit class EqualOps[A](a: A) {
+    def ===(other: A)(implicit equal: Equal[A]): Boolean =
+      equal.equal(a, other)
+  }
+}
+
+object CaseInsensitive extends Equal[String] {
+  override def equal(a1: String, a2: String): Boolean = a1.toLowerCase == a2.toLowerCase
+}
